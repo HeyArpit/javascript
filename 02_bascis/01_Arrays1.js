360b4ecaad47d6d3b8dd0edcff9955fa73042f73
Arpit Yadav <114281019+HeyArpit@users.noreply.github.com>
2024-11-03 10:20:50 +0000

    array part 1

---
+++ 02_bascis/01_Arrays1.js
@@ -0,0 +1,28 @@
+
+let Arr = [1, 2, 3, 4, 5]
+// console.log(Arr)
+// console.log(typeof Arr);
+// console.log(Arr[1]);
+
+
+let myArr = new Array(8, 7, 6, 5, 4 )
+// console.log(myArr);
+
+
+
+//Array Methods 
+
+// myArr.push(3)
+// myArr.push(2)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+let myNewArray = myArr.join()   //here join will create a array but it's type of will be string
+console.log(myNewArray)
+console.log(myArr);
+
+console.log(typeof myNewArray);
+console.log(typeof myArr);
+
